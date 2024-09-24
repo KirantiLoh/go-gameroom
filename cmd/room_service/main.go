bebb6aa2a10c9d0cdbbf8ff884d5cf75823b8358
KirantiLoh <mauriceyangmy28@gmail.com>
2024-09-24 14:18:02 +0700

    feat: create new main.go for room service

--- cmd/room_service/main.go
+++ cmd/room_service/main.go
@@ -4,11 +4,18 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/kirantiloh/gameroom/config"
+	"github.com/kirantiloh/gameroom/internal/api/rooms"
 )
 
 func main() {
 	r := chi.NewRouter()
 
+	config.LoadConfig()
+  roomRoutes := chi.NewRouter()
+  rooms.SetupRoutes(roomRoutes)
+
+  r.Mount("/rooms", roomRoutes)
 
 	http.ListenAndServe(":8001", r)
 }
