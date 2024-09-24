b7ea095dcb1570b79ac29509e77a7470915d5eb1
KirantiLoh <mauriceyangmy28@gmail.com>
2024-09-24 14:10:45 +0700

    refactor: removing migrations folder

--- internal/users/migrations/000001_create_users_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-  name varchar(100) not null,
-  email varchar(150) unique not null,
-  password varchar(200) not null,
-  is_verified boolean default false,
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
-);
